ecf2c17feadec860ac3496ff4a431a05c36891f6
Virgil Rosalita <shekinahberry143@gmail.com>
2021-09-07 19:48:23 +0800

    Initializing the project

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Current Location Areas</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/index.js"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDOYa5S7fyRsUTUgTIhu0McJTY1uA7ENP0&callback=initMap&libraries=&v=weekly" async></script>
+  </body>      
+</html>
